3d5192ae4b4a4eb7c88716eb81df3bb0b22bd255
nora.attobra <nora.attobra@gmail.com>
2020-07-31 23:10:49 -0400

    finish app

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Idea from './Idea'
 import update from 'immutability-helper'
+import IdeaForm from './IdeaForm'
 
 class IdeasContainer extends Component {
   
   constructor(props) {
   super(props)
   this.state = {
-    ideas: []
+    ideas: [],
+    editingIdeaId: null,
+    notification: ''
   }
 }
   
@@ -37,11 +40,41 @@ class IdeasContainer extends Component {
     const ideas = update(this.state.ideas, {
       $splice: [[0, 0, response.data]]
     })
-    this.setState({ideas: ideas})
+    this.setState({ideas: ideas,
+                  editingIdeaId: response.data.id})
   })
   .catch(error => console.log(error))
 }
   
+    
+    updateIdea = (idea) => {
+  const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+  const ideas = update(this.state.ideas, {
+    [ideaIndex]: { $set: idea }
+  })
+  this.setState({ideas: ideas,
+                notification: 'All changes saved'})
+}
+    
+  
+   resetNotification = () => {
+    this.setState({notification: ''})
+  }
+    
+   enableEditing = (id) => {
+    this.setState({editingIdeaId: id},
+    () => { this.title.focus() })
+  }
+   
+   deleteIdea = (id) => {
+    axios.delete(`https://titus-ideaboard.herokuapp.com/api/v1/ideas/${id}`)
+    .then(response => {
+      const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+      const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+      this.setState({ideas: ideas})
+    })
+    .catch(error => console.log(error))
+  }
   
   render() {
   return (
@@ -49,11 +82,22 @@ class IdeasContainer extends Component {
     <button className="newIdeaButton" onClick={this.addNewIdea}>
      New Idea
     </button>
+    
+    <span className="notification">
+  {this.state.notification}
+</span> 
+    
     <br />
     
      {this.state.ideas.map((idea) => {
-       return (<Idea idea={idea} key={idea.id} />)
-    })}
+  if(this.state.editingIdeaId === idea.id) {
+    return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} 
+           titleRef= {input => this.title = input} resetNotification={this.resetNotification} />)
+  } else {
+    return (<Idea idea={idea} key={idea.id} onClick={this.enableEditing} 
+           onDelete={this.deleteIdea} />)
+  }
+})}
     </div>
   );
 }
